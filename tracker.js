180e7b4cfbbc337e7eeec09eba61279fb4b9856e
wk645 <wk645@nyu.edu>
2017-10-25 12:50:29 -0400

    added notes and focus saves in storage

--- tracker.js
+++ tracker.js
@@ -1,36 +1,46 @@
-// $(function(){
-// 	$('#name').keyup(function(){
-// 		$('#greet').text('Hello ' + $('#name').val());
-// 	})
-// })
-
 document.addEventListener('DOMContentLoaded', function() {
 
+	document.body.onload = function() {
+		chrome.storage.sync.get('todo', function(data) {
+			document.getElementById('today').innerText = `
+			
+			${data.todo}`
+			console.log(storedFocus)
+		})
+	}
+
 	const todos = []
+	const storedFocus = []
 
 	let focusContent = document.getElementById('focusText')
+	// focusContent.style.display = 'block';
 	focusContent.addEventListener("keydown", function(event) {
 
-		// debugger
-		// var key = e.which || e.keyCode;
 		if (event.keyCode === 13) {
+
+			event.preventDefault();
 
 			let todayLi = document.getElementById('focusText')
 			todayObj = {}
 			todayObj.item = todayLi.value
 			todos.push(todayObj)
 
+			
+			chrome.storage.sync.set({'todo': focusContent.value}, function() {
+				alert("success!")
+			});
+
 			let mappedTodos = todos.map(todo => {
 				return `
 					<br>
-					<h2 id="todos">${todo.item} <button type="delete">X</button></h2>
+					<h2 id="todos">${todo.item} <button class="ui button">X</button></h2>
 				`
 			}).join("");
 
 			let todosList = document.getElementById("today")
 			todosList.innerHTML = mappedTodos
 			
-			focusContent.value = ""
+			focusContent.style.display = 'none';
 
 		}
 
@@ -42,9 +52,47 @@ document.addEventListener('DOMContentLoaded', function() {
 		let h = today.getHours();
 		let m = today.getMinutes();
 
-		time.innerHTML = `${h}:${m}`
+		if (m < 10) {
+			time.innerHTML = `${h}:0${m}`
+		} else {
+			time.innerHTML = `${h}:${m}`
+		}
 	}
 	setInterval(updateTime, 500);
+
+	const notes = []
+
+	let noteContainer = document.getElementById("noteText")
+	noteContainer.addEventListener("keydown", function(event) {
+
+		if (event.keyCode === 13) {
+
+			event.preventDefault();
+
+			let noteLi = document.getElementById("noteText")
+			noteObj = {}
+			noteObj.item = noteLi.value
+			notes.push(noteObj)
+
+			let mappedNotes = notes.map(note => {
+				return `
+					<br>
+					<li id="note">${note.item}</li>
+				`
+			}).join("");
+
+			let notesList = document.getElementById("allNotes")
+			notesList.innerHTML = mappedNotes;
+
+			noteContainer.value = "";
+		}
+
+		if (notes.length === 5) {
+			// make a scroll bar
+		}
+		
+	})
+
 
 
 
